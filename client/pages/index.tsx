25628629d89c256ff6d78d2b8a07dcb10f3a94c2
Ismaila Hassan <code.ismailah28@gmail.com>
2021-07-13 17:52:53 +0100

    add favicon and title

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,30 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>KEETS</h1>;
+  return (
+    <>
+      <Head>
+        <link rel='shortcut icon' href='/favicon/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicon/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon/favicon-16x16.png'
+        />
+        <title>KEETS Board</title>
+      </Head>
+      <h1>KEETS</h1>
+    </>
+  );
 }
